5ed5b577fffa2d25ecd6848ce19c4692ae97b671
Dennis Lee <leedennis04@gmail.com>
2020-06-29 12:25:50 -0500

    updated index.html, main_script.js & main_styles.css file

--- main_script.js
+++ main_script.js
@@ -1,16 +1,27 @@
 $(document).ready(function(){
     // console.log("document ready");
 
+    $("#header a").hover(
+        function() {
+            $(this).css("color", "blue");
+        }, function() {
+            $(this).css("color", "white");
+        }
+    );
+
     $("#frontEnd p").click(function(){
         $(this).siblings().slideToggle();
     });
 
-    $("#backEnd p").hover(
+    $("#backEnd p").click(function(){
+        $(this).siblings().slideToggle();
+    });
+
+    $("#snowboard").hover(
         function() {
-            $(this).siblings().slideToggle();
+            $(this).attr("src", "img/snowboarding_mountain2.jpg");
         }, function() {
-            $(this).siblings().slideToggle();
+            $(this).attr("src", "img/snowboarding_mountain.jpg");
         }
     );
-
 });
